4624fab9554296356b944d5f3e0da5e3d351d093
ethankeystone <ethankeystone@live.com>
2021-06-30 10:42:33 -0700

    added assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Chess_Board from './assets/Chess_Board.svg';
+
 function App() {
   return (
     <div className="App">
-      <img src={Chess_Board}></img>
+     
     </div>
   );
 }
---
+++ src/components/Chess.js
@@ -0,0 +1,9 @@
+function Chess() {
+    return (
+      <div className="App">
+          
+      </div>
+    );
+  }
+
+  export default Chess;
