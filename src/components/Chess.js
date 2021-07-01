d4b3eabcb8d2459e4478254f7317cd04e8d7372e
Ethan Xie <ethankeystone@live.com>
2021-07-01 15:06:38 -0700

    working static chess board

--- src/components/Chess.js
+++ src/components/Chess.js
@@ -2,13 +2,17 @@ import Board from "./graphics/Board.js";
 
 import "./Chess.css";
 
-
-
+let default_board = [];
+default_board.push([-5, -3, -4, -9, -15, -4, -3, -5]);
+default_board.push([-1, -1, -1, -1, -1, -1, -1, -1]);
+for (let i = 0; i < 4; i++) {
+  default_board.push([0, 0, 0, 0, 0, 0, 0, 0]);
+}
+default_board.push([1, 1, 1, 1, 1, 1, 1, 1]);
+default_board.push([5, 3, 4, 9, 15, 4, 3, 5]);
 function Chess() {
     let grid = [];
-    for (let i = 0; i < 8; i ++) {
-      grid.push([0, 0, 0, 0, 0, 0, 0, 0]);
-    };
+    grid = default_board;
     return (
       <div className="center">
           <Board  board={grid}></Board>
--- src/components/graphics/Board.js
+++ src/components/graphics/Board.js
@@ -3,6 +3,17 @@ import React from 'react';
 import "./Board.css";
 
 import white_pawn from "../../assets/white_pawn.svg";
+import white_knight from "../../assets/white_knight.svg";
+import white_bishop from "../../assets/white_bishop.svg";
+import white_rook from "../../assets/white_rook.svg";
+import white_queen from "../../assets/white_queen.svg";
+import white_king from "../../assets/white_king.svg";
+import black_pawn from "../../assets/black_pawn.svg";
+import black_knight from "../../assets/black_knight.svg";
+import black_bishop from "../../assets/black_bishop.svg";
+import black_rook from "../../assets/black_rook.svg";
+import black_queen from "../../assets/black_queen.svg";
+import black_king from "../../assets/black_king.svg";
 
 let VERTICAL_AXIS = ["1", "2", "3", "4", "5", "6", "7", "8"];
 let HORIZONTAL_AXIS = ["A", "B", "C", "D", "E", "F", "G", "H"];
@@ -13,31 +24,58 @@ export default function Board(props) {
         for (let i = 0; i < HORIZONTAL_AXIS.length; i++) {
             let number  = j + i + 2;
             let src = "";
-            if (props.board[i][j] == 0) {
+            let piece_exist = true;
+            let current_pos = props.board[j][i];
+            if (current_pos === 0) {
+                piece_exist = false;
+            } else if (current_pos === 1) {
                 src = white_pawn;
-            } else if (props.board[i][j] == 1) {
-                src = "./../../assets/white_pawn.svg";
-            } else if (props.board[i][j] == 3) {
-
-            } else if (props.board[i][j] == 4) {
-
-            } else if (props.board[i][j] == 5) {
-
-            } else if (props.board[i][j] == 9) {
-
-            } else if (props.board[i][j] == 15) {
-
+            } else if (current_pos === 3) {
+                src = white_knight;
+            } else if (current_pos === 4) {
+                src = white_bishop;
+            } else if (current_pos === 5) {
+                src = white_rook;
+            } else if (current_pos === 9) {
+                src = white_queen;
+            } else if (current_pos === 15) {
+                src = white_king;
+            } else if (current_pos === -1) {
+                src = black_pawn;
+            } else if (current_pos === -3) {
+                src = black_knight;
+            } else if (current_pos === -4) {
+                src = black_bishop;
+            } else if (current_pos === -5) {
+                src = black_rook;
+            } else if (current_pos === -9) {
+                src = black_queen;
+            } else if (current_pos === -15) {
+                src = black_king;
             }
 
-            if (number % 2 === 0) {
-                board.push(
-                    <div className="tile white-tile" key={VERTICAL_AXIS[i] + HORIZONTAL_AXIS[j]}> <img src={src}></img> </div>
-                );
+            if (piece_exist) {
+                if (number % 2 === 0) {
+                    board.push(
+                        <div className="tile white-tile" key={VERTICAL_AXIS[i] + HORIZONTAL_AXIS[j]}> <img className="tile" src={src}></img> </div>
+                    );
+                } else {
+                    board.push(
+                        <div className="tile black-tile" key={VERTICAL_AXIS[i] + HORIZONTAL_AXIS[j]}> <img className="tile" src={src}></img> </div>
+                    );
+                }
             } else {
-                board.push(
-                    <div className="tile black-tile" key={VERTICAL_AXIS[i] + HORIZONTAL_AXIS[j]}> <img src={src}></img> </div>
-                );
+                if (number % 2 === 0) {
+                    board.push(
+                        <div className="tile white-tile" key={VERTICAL_AXIS[i] + HORIZONTAL_AXIS[j]}> </div>
+                    );
+                } else {
+                    board.push(
+                        <div className="tile black-tile" key={VERTICAL_AXIS[i] + HORIZONTAL_AXIS[j]}> </div>
+                    );
+                }
             }
+        
         }
       }
     return (
